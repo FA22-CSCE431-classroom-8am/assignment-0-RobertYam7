23e30b607caea14f3d124959b2f1f10156797061
RobertYam7 <robert8y@gmail.com>
2022-09-13 23:45:48 -0500

    added unit and integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,9 +4,33 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'To Kill a Mockingbird'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('To Kill a Mockingbird')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'Harper Lee'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Harper Lee')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: '7'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('7')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Date', with: '9-13-2022'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('9-13-2022')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'To Kill a Mockingbird')
   end
 
   it 'is valid with valid attributes' do
@@ -11,6 +11,45 @@ RSpec.describe Book, type: :model do
   end
 
   it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  subject do
+    described_class.new(author: 'Harper Lee')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  subject do
+    described_class.new(price: 7)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  subject do
+    described_class.new(date: '9-13-2022')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a date' do
     subject.title = nil
     expect(subject).not_to be_valid
   end
