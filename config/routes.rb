90235d9b2c6259a6ec83fcaa5648157a0a423f2b
RobertYam7 <robert8y@gmail.com>
2022-09-14 00:38:53 -0500

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/new'
+  get 'books/show'
+  get 'books/create'
+  get 'books/update'
+  get 'books/delete'
+
   resources :books
   root 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
